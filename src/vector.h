1e98af6a52485883ec8592e638a83a5d8671fe2d
Jaak Ristioja <jaak.ristioja@cyber.ee>
2011-05-30 12:49:51 +0300

    Made generic vectors use size_t and added checks for possible size_t overflows.

--- src/vector.h
+++ src/vector.h
@@ -6,17 +6,17 @@
         specify linking constraints etc).
 */
 
-#define SVM_VECTOR_DECLARE(name,datatype,sizetype) \
+#define SVM_VECTOR_DECLARE(name,datatype) \
     struct name { \
-        sizetype size; \
+        size_t size; \
         datatype * data; \
     }; \
     struct name * name ## _new (); \
-    struct name * name ## _new2 (sizetype size); \
+    struct name * name ## _new2 (size_t size); \
     void name ## _free(struct name * const r); \
-    int name ## _resize(struct name * const r, const sizetype newSize);
+    int name ## _resize(struct name * const r, const size_t newSize);
 
-#define SVM_VECTOR_DEFINE(name,datatype,sizetype) \
+#define SVM_VECTOR_DEFINE(name,datatype) \
     void name ## _init(struct name * const r) { \
         r->size = 0; \
         r->data = NULL; \
@@ -29,12 +29,15 @@
         } \
         return r; \
     } \
-    struct name * name ## _new2(sizetype size) { \
+    struct name * name ## _new2(size_t size) { \
+        size_t realSize = size * sizeof(datatype); \
+        if (realSize / sizeof(datatype) != size) \
+            return NULL; \
         struct name * const r = malloc(sizeof(struct name)); \
         if (!r) \
             goto name ## _new2_fail_1; \
         if (size > 0) { \
-            r->data = calloc(size, sizeof(datatype)); \
+            r->data = malloc(realSize); \
             if (!r->data) \
                 goto name ## _new2_fail_2; \
             r->size = size; \
@@ -47,19 +50,22 @@
     name ## _new2_fail_2: \
         free(r); \
     name ## _new2_fail_1: \
-        return 0; \
+        return NULL; \
     } \
     void name ## _free(struct name * const r) { \
         assert(r); \
         free(r->data); \
         free(r); \
     } \
-    int name ## _resize(struct name * const r, const sizetype newSize) { \
+    int name ## _resize(struct name * const r, const size_t newSize) { \
         assert(r); \
+        size_t realSize = newSize * sizeof(datatype); \
+        if (realSize / sizeof(datatype) != newSize) \
+            return 0; \
         if (r->size == newSize) \
             return 1; \
         /** \todo check size_t overflow. */ \
-        datatype * const d = realloc(r->data, sizeof(datatype) * newSize); \
+        datatype * const d = realloc(r->data, realSize); \
         if (!d) \
             return 0; \
         r->data = d; \
