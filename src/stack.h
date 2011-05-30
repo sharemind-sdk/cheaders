344a8001b4d14abf41bc42c1b4361203ddd729ce
Jaak Ristioja <jaak.ristioja@cyber.ee>
2011-05-30 11:32:58 +0300

    Added generic vectors and stacks.

---
+++ src/stack.h
@@ -0,0 +1,62 @@
+#ifndef STACK_H
+#define STACK_H
+
+/**
+  \todo Allow for more fine-grained declarations and definitions (e.g. to
+        specify linking constraints etc).
+*/
+
+#define SVM_STACK_DECLARE(name,datatype) \
+    struct name ## _item; \
+    struct name { \
+        struct name ## _item * d; \
+    }; \
+    struct name * name ## _new (); \
+    void name ## _free (struct name * s); \
+    datatype * name ## _push (struct name * s); \
+    void name ## _pop (struct name * s);
+
+#define SVM_STACK_DEFINE(name,datatype) \
+    struct name ## _item { \
+        datatype value; \
+        struct name ## _item * prev; \
+    }; \
+    struct name * name ## _new () { \
+        struct name * s = malloc(sizeof(struct name)); \
+        if (s) \
+            s->d = NULL; \
+        return s; \
+    } \
+    void name ## _free (struct name * s) { \
+        assert(s); \
+        struct name ## _item * d = s->d; \
+        if (d) { \
+            struct name ## _item * prev = d->prev; \
+            for (;;) { \
+                free(d); \
+                if (!prev) \
+                    break; \
+                d = prev; \
+                prev = d->prev; \
+            } \
+        } \
+        free(s); \
+    } \
+    datatype * name ## _push (struct name * s) { \
+        assert(s); \
+        struct name ## _item * n = malloc(sizeof(struct name ## _item)); \
+        if (!n) \
+            return NULL; \
+        n->prev = s->d; \
+        s->d = n; \
+        return &n->value; \
+    } \
+    void name ## _pop (struct name * s) { \
+        assert(s); \
+        struct name ## _item * d = s->d; \
+        assert(d); \
+        s->d = d->prev; \
+        free(d); \
+    }
+
+#endif /* STACK_H */
---
+++ src/vector.h
@@ -0,0 +1,61 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#define SVM_VECTOR_DECLARE(name,datatype,sizetype) \
+    struct name { \
+        sizetype size; \
+        datatype * data; \
+    }; \
+    struct name * name ## _new (); \
+    struct name * name ## _new2 (sizetype size); \
+    void name ## _free(struct name * const r); \
+    int name ## _resize(struct name * const r, const sizetype newSize);
+
+#define SVM_VECTOR_DEFINE(name,datatype,sizetype) \
+    struct name * name ## _name () { \
+        struct name * const r = malloc(sizeof(struct name)); \
+        if (r) { \
+            r->size = 0; \
+            r->data = NULL; \
+        } \
+        return r; \
+    } \
+    struct name * name ## _new2(sizetype size) { \
+        struct name * const r = malloc(sizeof(struct name)); \
+        if (!r) \
+            goto name ## _new2_fail_1; \
+        if (size > 0) { \
+            r->data = calloc(size, sizeof(datatype)); \
+            if (!r->data) \
+                goto name ## _new2_fail_2; \
+            r->size = size; \
+        } else { \
+            r->data = NULL; \
+            r->size = 0; \
+        } \
+    /* SUCCESS: */ \
+        return r; \
+    name ## _new2_fail_2: \
+        free(r); \
+    name ## _new2_fail_1: \
+        return 0; \
+    } \
+    void name ## _free(struct name * const r) { \
+        assert(r); \
+        free(r->data); \
+        free(r); \
+    } \
+    int name ## _resize(struct name * const r, const sizetype newSize) { \
+        assert(r); \
+        if (r->size == newSize) \
+            return 1; \
+        /** \todo check size_t overflow. */ \
+        datatype * const d = realloc(r->data, sizeof(datatype) * newSize); \
+        if (!d) \
+            return 0; \
+        r->data = d; \
+        r->size = newSize; \
+        return 1; \
+    }
+
+#endif /* VECTOR_H */
