081873e104a2292e7eea57814f00b1dc92481a9e
Jaak Ristioja <jaak.ristioja@cyber.ee>
2011-11-10 19:02:57 +0200

    Moved syscall management from vm to libsmvm, modularized modapi.

--- src/modapi_0x1.h
+++ src/modapi_0x1.h
@@ -9,13 +9,82 @@
 #ifndef SHAREMIND_MODAPI_0x1_H
 #define SHAREMIND_MODAPI_0x1_H
 
-#include "libsmvm/syscall.h"
+#include <stddef.h>
+#include <stdint.h>
+#include "codeblock.h"
 #include "modapi.h"
 
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct {
+    void * pData;
+    size_t size;
+} SMVM_MODAPI_0x1_Reference;
+typedef struct {
+    const void * pData;
+    size_t size;
+} SMVM_MODAPI_0x1_CReference;
+
+typedef enum {
+    SMVM_MODAPI_0x1_E_NONE = 0x00,
+    SMVM_MODAPI_0x1_E_OUT_OF_MEMORY = 0x01,
+    SMVM_MODAPI_0x1_E_INVALID_CALL = 0x02
+} SMVM_MODAPI_0x1_Syscall_Exception;
+
+/** Additional context provided for system calls: */
+struct _SMVM_MODAPI_0x1_Syscall_Context;
+typedef struct _SMVM_MODAPI_0x1_Syscall_Context SMVM_MODAPI_0x1_Syscall_Context;
+struct _SMVM_MODAPI_0x1_Syscall_Context {
+    /**
+      A handle to the private data of the module instance. This is the same
+      handle as provided to SMVM_module_context on module initialization.
+    */
+    void * const moduleHandle;
+
+    /** Access to public dynamic memory inside the VM process: */
+    uint64_t (* const publicAlloc)(size_t nBytes, SMVM_MODAPI_0x1_Syscall_Context * c);
+    int (* const publicFree)(uint64_t ptr, SMVM_MODAPI_0x1_Syscall_Context * c);
+    size_t (* const publicMemPtrSize)(uint64_t ptr, SMVM_MODAPI_0x1_Syscall_Context * c);
+    void * (* const publicMemPtrData)(uint64_t ptr, SMVM_MODAPI_0x1_Syscall_Context * c);
+
+    /** Access to dynamic memory not exposed to VM instructions: */
+    void * (* const allocPrivate)(size_t nBytes, SMVM_MODAPI_0x1_Syscall_Context * c);
+    int (* const freePrivate)(void * ptr, SMVM_MODAPI_0x1_Syscall_Context * c);
+
+    /**
+      Used to get access to internal data of protection domain per-process data
+      (see below for pdProcessHandle):
+    */
+    void * (* const get_pd_process_handle)(uint64_t pd_index,
+                                           SMVM_MODAPI_0x1_Syscall_Context * p);
+
+    /* OTHER STUFF */
+};
+
+typedef SMVM_MODAPI_0x1_Syscall_Exception (* SMVM_MODAPI_0x1_Syscall)(
+    /** Arguments passed to syscall: */
+    SMVM_CodeBlock * args,
+    size_t num_args,
+
+    /** Mutable references passed to syscall: */
+    const SMVM_MODAPI_0x1_Reference * refs,
+    size_t num_refs,
+
+    /** Immutable references passed to syscall: */
+    const SMVM_MODAPI_0x1_CReference * crefs,
+    size_t num_crefs,
+
+    /**
+      The pointer to where the return value of the syscall resides, or NULL if
+      no return value is expected:
+    */
+    SMVM_CodeBlock * returnValue,
+
+    SMVM_MODAPI_0x1_Syscall_Context * c
+);
 
 typedef enum {
     SMVM_MODAPI_0x1_OK = 0,
@@ -50,8 +119,8 @@ typedef void (*SMVM_MODAPI_0x1_Module_Deinitializer)(SMVM_MODAPI_0x1_Module_Cont
 /** System call list item:*/
 typedef const struct {
     const char * const name;
-    const SMVM_syscall_f function_handle;
-} SMVM_MODAPI_0x1_Syscall;
+    const SMVM_MODAPI_0x1_Syscall function_handle;
+} SMVM_MODAPI_0x1_Syscall_Definition;
 
 
 #ifdef __cplusplus
