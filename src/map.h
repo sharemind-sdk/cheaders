71843e5cae66aa6009db9f58f5de2c0689f77a5d
Jaak Ristioja <jaak.ristioja@cyber.ee>
2011-11-10 11:29:31 +0200

    Enhanced map.

--- src/map.h
+++ src/map.h
@@ -10,6 +10,8 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <stddef.h>
+#include <stdint.h>
 #include "likely.h"
 
 
@@ -26,6 +28,54 @@
 #define SM_MAP_EXTERN_C_END
 #endif
 
+#define SM_MAP_KEY_EQUALS_DEFINE(name,keytype) \
+    inline int name(keytype const k1, keytype const k2) { return k1 == k2; }
+
+#define SM_MAP_KEY_LESS_THAN_DEFINE(name,keytype) \
+    inline int name(keytype const k1, keytype const k2) { return k1 < k2; }
+
+#define _SM_MAP_KEY_COMPARATORS_DEFINE(keytype_str,keytype) \
+    SM_MAP_KEY_EQUALS_DEFINE(SM_MAP_KEY_EQUALS_ ## keytype_str, keytype) \
+    SM_MAP_KEY_LESS_THAN_DEFINE(SM_MAP_KEY_LESS_THAN_ ## keytype_str, keytype)
+
+#define SM_MAP_KEY_COMPARATORS_DEFINE(keytype) _SM_MAP_KEY_COMPARATORS_DEFINE(keytype,keytype)
+
+SM_MAP_KEY_COMPARATORS_DEFINE(char)
+_SM_MAP_KEY_COMPARATORS_DEFINE(signed_char,signed char)
+_SM_MAP_KEY_COMPARATORS_DEFINE(unsigned_char,unsigned char)
+SM_MAP_KEY_COMPARATORS_DEFINE(short)
+_SM_MAP_KEY_COMPARATORS_DEFINE(unsigned_short,unsigned short)
+SM_MAP_KEY_COMPARATORS_DEFINE(int)
+SM_MAP_KEY_COMPARATORS_DEFINE(unsigned)
+_SM_MAP_KEY_COMPARATORS_DEFINE(unsigned_int,unsigned int)
+SM_MAP_KEY_COMPARATORS_DEFINE(long)
+_SM_MAP_KEY_COMPARATORS_DEFINE(unsigned_long,unsigned long)
+SM_MAP_KEY_COMPARATORS_DEFINE(size_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int8_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int16_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int32_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int64_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint8_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint16_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint32_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint64_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int_fast8_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int_fast16_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int_fast32_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int_fast64_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint_fast8_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint_fast16_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint_fast32_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint_fast64_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int_least8_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int_least16_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int_least32_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(int_least64_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint_least8_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint_least16_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint_least32_t)
+SM_MAP_KEY_COMPARATORS_DEFINE(uint_least64_t)
+
 #define SM_MAP_DECLARE(name,keytype,valuetype,inlinePerhaps) \
     SM_MAP_EXTERN_C_BEGIN \
     struct name ## _item; \
@@ -41,7 +91,7 @@
     inlinePerhaps valuetype * name ## _get (name * s, keytype key) __attribute__ ((nonnull(1), warn_unused_result)); \
     SM_MAP_EXTERN_C_END
 
-#define SM_MAP_DEFINE(name,keytype,valuetype,keyhashfunction,mymalloc,myfree,inlinePerhaps) \
+#define SM_MAP_DEFINE(name,keytype,valuetype,keyhashfunction,keyequals,keylessthan,mymalloc,myfree,inlinePerhaps) \
     SM_MAP_EXTERN_C_BEGIN \
     struct name ## _item { \
         keytype key; \
@@ -96,9 +146,9 @@
         struct name ## _item * p; \
         if (*l) { \
             do { \
-                if ((*l)->key > key) \
+                if (keylessthan(key, (*l)->key)) \
                     break; \
-                if ((*l)->key == key) \
+                if (keyequals(key, (*l)->key)) \
                     return &(*l)->value; \
                 l = &(*l)->next; \
             } while (*l); \
@@ -121,12 +171,12 @@
         struct name ## _item ** prevPtr = &s->d[hash]; \
         struct name ## _item * l = *prevPtr; \
         while (l) { \
-            if (l->key == key) { \
+            if (keyequals(key, l->key)) { \
                 *prevPtr = l->next; \
                 myfree(l); \
                 return 1; \
             } \
-            if (l->key > key) \
+            if (keylessthan(key, l->key)) \
                 return 0; \
             prevPtr = &l->next; \
             l = *prevPtr; \
@@ -138,9 +188,9 @@
         uint16_t hash = keyhashfunction(key); \
         struct name ## _item * l = s->d[hash]; \
         while (l) { \
-            if (l->key == key) \
+            if (keyequals(key, l->key)) \
                 return &l->value; \
-            if (l->key > key) \
+            if (keylessthan(key, l->key)) \
                 return NULL; \
             l = l->next; \
         } \
