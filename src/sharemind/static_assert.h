8d181bd9a7e9d0f6a0eb050dc0d57395f799499d
Jaak Ristioja <jaak.ristioja@cyber.ee>
2013-01-02 14:17:19 +0200

    Make SHAREMIND_STATIC_ASSERT work properly with C++.

--- src/sharemind/static_assert.h
+++ src/sharemind/static_assert.h
@@ -10,8 +10,17 @@
 #ifndef SHAREMIND_STATIC_ASSERT_H
 #define SHAREMIND_STATIC_ASSERT_H
 
+#ifdef __cplusplus
+
+#include <boost/static_assert.hpp>
+#define SHAREMIND_STATIC_ASSERT(...) BOOST_STATIC_ASSERT(__VA_ARGS__)
+
+#else
+
 #define SHAREMIND_STATIC_ASSERT_CAT2(a,b) a ## b
 #define SHAREMIND_STATIC_ASSERT_CAT(a,b) SHAREMIND_STATIC_ASSERT_CAT2(a,b)
 #define SHAREMIND_STATIC_ASSERT(cond) typedef char SHAREMIND_STATIC_ASSERT_CAT(static_assertion_,__COUNTER__)[(cond) ? 1 : -1]
+
+#endif
 
 #endif /* SHAREMIND_STATIC_ASSERT */
