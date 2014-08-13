b782514a2c9546a1a5fcba17dfbd11692fef8237
Jaak Ristioja <jaak.ristioja@cyber.ee>
2014-08-13 15:58:40 +0300

    Static assert enhancements.

--- src/static_assert.h
+++ src/static_assert.h
@@ -12,14 +12,33 @@
 
 #ifdef __cplusplus
 
+#if __cplusplus >= 201103L
+
+#define SHAREMIND_STATIC_ASSERT(...) static_assert(__VA_ARGS__, #__VA_ARGS__)
+
+#else
+
 #include <boost/static_assert.hpp>
 #define SHAREMIND_STATIC_ASSERT(...) BOOST_STATIC_ASSERT(__VA_ARGS__)
+
+#endif
+
+#else
+
+#if __STDC_VERSION__ >= 201112L
+
+#define SHAREMIND_STATIC_ASSERT(...) _Static_assert(__VA_ARGS__)
 
 #else
 
 #define SHAREMIND_STATIC_ASSERT_CAT2(a,b) a ## b
 #define SHAREMIND_STATIC_ASSERT_CAT(a,b) SHAREMIND_STATIC_ASSERT_CAT2(a,b)
-#define SHAREMIND_STATIC_ASSERT(cond) typedef char SHAREMIND_STATIC_ASSERT_CAT(static_assertion_,__COUNTER__)[(cond) ? 1 : -1]
+#define SHAREMIND_STATIC_ASSERT(cond) \
+    typedef char SHAREMIND_STATIC_ASSERT_CAT(\
+            static_assertion_,\
+            __COUNTER__)[(cond) ? 1 : -1] __attribute__((unused))
+
+#endif
 
 #endif
 
