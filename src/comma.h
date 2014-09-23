0a4d15a8914d01752ea88d1f0189eb31fe994dae
Jaak Ristioja <jaak.ristioja@cyber.ee>
2014-09-23 16:45:18 +0300

    Added SHAREMIND_COMMA macro.

---
+++ src/comma.h
@@ -0,0 +1,15 @@
+/*
+ * This file is a part of the Sharemind framework.
+ * Copyright (C) Cybernetica AS
+ *
+ * All rights are reserved. Reproduction in whole or part is prohibited
+ * without the written consent of the copyright owner. The usage of this
+ * code is subject to the appropriate license agreement.
+ */
+
+#ifndef SHAREMIND_COMMA_H
+#define SHAREMIND_COMMA_H
+
+#define SHAREMIND_COMMA ,
+
+#endif /* SHAREMIND_COMMA_H */
