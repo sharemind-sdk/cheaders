a8093e8ab507eeb4be7be9fc0e80bff94e623aff
Jaak Ristioja <jaak.ristioja@cyber.ee>
2014-09-26 14:10:54 +0300

    Implemented a simple SHAREMIND_WRAP preprocessor macro.

---
+++ src/wrap.h
@@ -0,0 +1,15 @@
+/*
+ * This file is a part of the Sharemind framework.
+ * Copyright (C) Cybernetica AS
+ *
+ * All rights are reserved. Reproduction in whole or part is prohibited
+ * without the written consent of the copyright owner. The usage of this
+ * code is subject to the appropriate license agreement.
+ */
+
+#ifndef SHAREMIND_WRAP_H
+#define SHAREMIND_WRAP_H
+
+#define SHAREMIND_WRAP(...) __VA_ARGS__
+
+#endif /* SHAREMIND_WRAP_H */
