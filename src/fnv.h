a7e1750d0db2dcbd9acf4deef5ad4e4188351853
Jaak Ristioja <jaak.ristioja@cyber.ee>
2015-05-21 12:54:46 +0300

    fnv.h: Removed register specifiers. Compiler inlining is smart enough.

--- src/fnv.h
+++ src/fnv.h
@@ -48,9 +48,9 @@ SHAREMIND_EXTERN_C_BEGIN
   \param[in] len length of buffer in octets
   \returns 64 bit hash value
 */
-inline uint64_t fnv_64a_buf(register const void * buf, register size_t len) {
+inline uint64_t fnv_64a_buf(const void * buf, size_t len) {
     assert(buf);
-    register uint64_t hval = UINT64_C(0xcbf29ce484222325); /* Initial hash recommended value */
+    uint64_t hval = UINT64_C(0xcbf29ce484222325); /* Initial hash recommended value */
     for (size_t i = 0u; i < len; i++) {
         hval ^= (uint64_t) ((const uint8_t *) buf)[i];
         hval *= UINT64_C(0x100000001b3);
@@ -63,9 +63,9 @@ inline uint64_t fnv_64a_buf(register const void * buf, register size_t len) {
   \param[in] str string to hash
   \returns 64 bit hash value
 */
-inline uint64_t fnv_64a_str(register const char * str) {
+inline uint64_t fnv_64a_str(const char * str) {
     assert(str);
-    register uint64_t hval = UINT64_C(0xcbf29ce484222325); /* Initial hash recommended value */
+    uint64_t hval = UINT64_C(0xcbf29ce484222325); /* Initial hash recommended value */
     while (*str) {
         hval ^= (uint64_t) *((const uint8_t *) str++);
         hval *= UINT64_C(0x100000001b3);
@@ -79,10 +79,10 @@ inline uint64_t fnv_64a_str(register const char * str) {
   \param[in] len length of buffer in octets
   \returns 32 bit hash value
 */
-inline uint32_t fnv_32a_buf(register const void * buf, register size_t len) {
+inline uint32_t fnv_32a_buf(const void * buf, size_t len) {
     assert(buf);
-    register uint32_t hval = UINT32_C(0x811c9dc5); /* Initial hash recommended value */
-    for (register size_t i = 0u; i < len; i++) {
+    uint32_t hval = UINT32_C(0x811c9dc5); /* Initial hash recommended value */
+    for (size_t i = 0u; i < len; i++) {
         hval ^= (uint32_t) ((const uint8_t *) buf)[i];
         hval *= UINT32_C(0x01000193);
     }
@@ -94,9 +94,9 @@ inline uint32_t fnv_32a_buf(register const void * buf, register size_t len) {
   \param[in] str string to hash
   \returns 32 bit hash value
 */
-inline uint32_t fnv_32a_str(register const char * str) {
+inline uint32_t fnv_32a_str(const char * str) {
     assert(str);
-    register uint32_t hval = UINT32_C(0x811c9dc5); /* Initial hash recommended value */
+    uint32_t hval = UINT32_C(0x811c9dc5); /* Initial hash recommended value */
     while (*str) {
         hval ^= (uint32_t) *((const uint8_t *) str++);
         hval *= UINT32_C(0x01000193);
@@ -109,7 +109,7 @@ inline uint32_t fnv_32a_str(register const char * str) {
   \param[in] buf buffer to hash
   \returns 16 bit hash value
 */
-inline uint16_t fnv_16a_buf(register const void * buf, register size_t len) {
+inline uint16_t fnv_16a_buf(const void * buf, size_t len) {
     assert(buf);
     uint32_t hval = fnv_32a_buf(buf, len);
     return (uint16_t) (hval ^ (hval >> 16));
@@ -120,7 +120,7 @@ inline uint16_t fnv_16a_buf(register const void * buf, register size_t len) {
   \param[in] str string to hash
   \returns 16 bit hash value
 */
-inline uint16_t fnv_16a_str(register const char * str) {
+inline uint16_t fnv_16a_str(const char * str) {
     assert(str);
     uint32_t hval = fnv_32a_str(str);
     return (uint16_t) (hval ^ (hval >> 16));
